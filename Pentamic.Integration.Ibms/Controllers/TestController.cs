889f618c7e8665fde302c5853d93576c05d11ce5
vutunglam <vutunglam@pentamic.vn>
2016-10-22 11:03:24 +0700

    Rename Package to Protocol

--- Pentamic.Integration.Ibms/Controllers/TestController.cs
+++ Pentamic.Integration.Ibms/Controllers/TestController.cs
@@ -35,20 +35,21 @@ namespace Pentamic.Integration.Ibms.Controllers
 
         [HttpPost]
         [Route("base64")]
-        public Package Base64([FromBody]string value)
+        public Protocol Base64([FromBody]string value)
         {
             Log.Information("Request: ,{Content-Type}, {Charset}", Request.Content.Headers.ContentType.MediaType, Request.Content.Headers.ContentType.CharSet);
             if (value == null)
             {
                 Log.Warning("Received null string");
-                return new Package
+                return new Protocol
                 {
-                    ProtocolId = 0,
-                    ProtocolStatus = false,
-                    Partner_name = "",
-                    Partner_pass = "",
-                    ErrCode = 0,
-                    ErrMessage = "Input string is null"
+                    protocol_id = 0,
+                    branch_id = 0,
+                    protocol_status = false,
+                    partner_name = "",
+                    partner_pass = "",
+                    err_code = 0,
+                    err_message = "Input string is null"
                 };
             }
             try
@@ -57,28 +58,30 @@ namespace Pentamic.Integration.Ibms.Controllers
                 var decodedData = Convert.FromBase64String(value);
                 var jsonString = Encoding.UTF8.GetString(decodedData);
                 Log.Information("Decoded json string from base64 {0}", jsonString);
-                var model = JsonConvert.DeserializeObject<Package>(jsonString);
-                return new Package
+                var model = JsonConvert.DeserializeObject<Protocol>(jsonString);
+                return new Protocol
                 {
-                    ProtocolId = model.ProtocolId,
-                    ProtocolStatus = true,
-                    Partner_name = "",
-                    Partner_pass = "",
-                    ErrCode = 0,
-                    ErrMessage = null
+                    protocol_id = model.protocol_id,
+                    branch_id = model.branch_id,
+                    protocol_status = true,
+                    partner_name = "",
+                    partner_pass = "",
+                    err_code = 0,
+                    err_message = null
                 };
             }
             catch (Exception e)
             {
                 Log.Error(e, "Error get package value");
-                return new Package
+                return new Protocol
                 {
-                    ProtocolId = 0,
-                    ProtocolStatus = false,
-                    Partner_name = "",
-                    Partner_pass = "",
-                    ErrCode = 0,
-                    ErrMessage = e.Message
+                    protocol_id = 0,
+                    branch_id = 0,
+                    protocol_status = false,
+                    partner_name = "",
+                    partner_pass = "",
+                    err_code = 0,
+                    err_message = e.Message
                 };
             }
         }
@@ -86,26 +89,26 @@ namespace Pentamic.Integration.Ibms.Controllers
 
         [HttpPost]
         [Route("json")]
-        public IHttpActionResult Json(Package protocol)
+        public IHttpActionResult Json(Protocol protocol)
         {
             Log.Information("Request: {MediaType}", Request.Content.Headers.ContentType.MediaType);
             Log.Information("Received package: {Package}", JsonConvert.SerializeObject(protocol));
-            var result = protocol ?? new Package
+            var result = protocol ?? new Protocol
             {
-                ProtocolStatus = false
+                protocol_status = false
             };
             return Ok(result);
         }
 
         [HttpPost]
         [Route("json2")]
-        public HttpResponseMessage Json2(Package package)
+        public HttpResponseMessage Json2(Protocol package)
         {
             Log.Information("Request: {MediaType}", Request.Content.Headers.ContentType.MediaType);
             Log.Information("Received package: {Package}", JsonConvert.SerializeObject(package));
-            var result = package ?? new Package
+            var result = package ?? new Protocol
             {
-                ProtocolStatus = false
+                protocol_status = false
             };
             return new HttpResponseMessage
             {
@@ -115,13 +118,13 @@ namespace Pentamic.Integration.Ibms.Controllers
 
         [HttpPost]
         [Route("json3")]
-        public Package Json3([FromBody]Package package)
+        public Protocol Json3([FromBody]Protocol package)
         {
             Log.Information("Request: {MediaType}", Request.Content.Headers.ContentType.MediaType);
             Log.Information("Received package: {Package}", JsonConvert.SerializeObject(package));
-            return package ?? new Package
+            return package ?? new Protocol
             {
-                ProtocolStatus = false
+                protocol_status = false
             };
         }
     }
---
+++ Pentamic.Integration.Ibms/Models/Protocol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pentamic.Integration.Ibms.Models
+{
+    public class Protocol
+    {
+        public int protocol_id { get; set; }
+        public int branch_id { get; set; }
+        public int err_code { get; set; }
+        public string err_message { get; set; }
+        public bool protocol_status { get; set; }
+        public string partner_name { get; set; }
+        public string partner_pass { get; set; }
+    }
+}
